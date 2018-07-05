9ddd38e06ae252d82a371a127575a3d9ce411781
Jan Michael Auer <jan.auer@sentry.io>
2018-07-05 09:44:40 +0200

    feat: Implement correct Annotated<Date> deserialization

--- src/utils.rs
+++ src/utils.rs
@@ -38,39 +38,12 @@ pub mod serde_chrono {
     use serde::{de, ser};
 
     use meta::Annotated;
+    use utils::serde::CustomDeserialize;
 
     pub fn timestamp_to_datetime(ts: f64) -> DateTime<Utc> {
         let secs = ts as i64;
         let micros = (ts.fract() * 1_000_000f64) as u32;
         Utc.timestamp_opt(secs, micros * 1000).unwrap()
-    }
-
-    pub fn deserialize<'de, D>(deserializer: D) -> Result<Annotated<DateTime<Utc>>, D::Error>
-    where
-        D: de::Deserializer<'de>,
-    {
-        Ok(deserializer
-            .deserialize_any(SecondsTimestampVisitor)?
-            .with_timezone(&Utc)
-            .into())
-    }
-
-    pub fn serialize<S>(value: &Annotated<DateTime<Utc>>, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: ser::Serializer,
-    {
-        // TODO: This will serialize an invalid payload!
-        let datetime = match value.value() {
-            Some(dt) => dt,
-            None => return serializer.serialize_none(),
-        };
-
-        if datetime.timestamp_subsec_nanos() == 0 {
-            serializer.serialize_i64(datetime.timestamp())
-        } else {
-            let micros = datetime.timestamp_subsec_micros() as f64 / 1_000_000f64;
-            serializer.serialize_f64(datetime.timestamp() as f64 + micros)
-        }
     }
 
     struct SecondsTimestampVisitor;
@@ -96,6 +69,85 @@ pub mod serde_chrono {
 
         fn visit_str<E: de::Error>(self, value: &str) -> Result<Self::Value, E> {
             value.parse().map_err(|e| E::custom(format!("{}", e)))
+        }
+    }
+
+    struct SerdeDateTime;
+
+    impl<'de> CustomDeserialize<'de, DateTime<Utc>> for SerdeDateTime {
+        fn deserialize<D>(deserializer: D) -> Result<DateTime<Utc>, D::Error>
+        where
+            D: de::Deserializer<'de>,
+        {
+            Ok(deserializer
+                .deserialize_any(SecondsTimestampVisitor)?
+                .with_timezone(&Utc))
+        }
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Annotated<DateTime<Utc>>, D::Error>
+    where
+        D: de::Deserializer<'de>,
+    {
+        Annotated::deserialize_with(deserializer, SerdeDateTime)
+    }
+
+    pub fn serialize<S>(value: &Annotated<DateTime<Utc>>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: ser::Serializer,
+    {
+        let datetime = match value.value() {
+            Some(dt) => dt,
+            None => return serializer.serialize_unit(),
+        };
+
+        if datetime.timestamp_subsec_nanos() == 0 {
+            serializer.serialize_i64(datetime.timestamp())
+        } else {
+            let micros = datetime.timestamp_subsec_micros() as f64 / 1_000_000f64;
+            serializer.serialize_f64(datetime.timestamp() as f64 + micros)
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        use serde_json::Deserializer;
+
+        #[test]
+        fn test_timestamp() {
+            let deserializer = &mut Deserializer::from_str("946684800");
+            assert_eq!(
+                deserialize(deserializer).unwrap(),
+                Annotated::from(Utc.ymd(2000, 1, 1).and_hms(0, 0, 0))
+            );
+        }
+
+        #[test]
+        fn test_date() {
+            let deserializer = &mut Deserializer::from_str("\"2000-01-01T00:00:00Z\"");
+            assert_eq!(
+                deserialize(deserializer).unwrap(),
+                Annotated::from(Utc.ymd(2000, 1, 1).and_hms(0, 0, 0))
+            );
+        }
+
+        #[test]
+        fn test_invalid_date() {
+            let deserializer = &mut Deserializer::from_str("\"invalid\"");
+            assert_eq!(
+                deserialize(deserializer).unwrap(),
+                Annotated::from_error("input contains invalid characters")
+            );
+        }
+
+        #[test]
+        fn test_invalid_type() {
+            let deserializer = &mut Deserializer::from_str("true");
+            assert_eq!(
+                deserialize(deserializer).unwrap(),
+                Annotated::from_error("invalid type: boolean `true`, expected a unix timestamp")
+            );
         }
     }
 }
