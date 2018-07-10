81470f689cd004362a689435e71906ec8a7eac1e
Jan Michael Auer <jan.auer@sentry.io>
2018-07-10 16:42:41 +0200

    feat: Implement the full Breadcrumb schema

--- src/lib.rs
+++ src/lib.rs
@@ -6,38 +6,33 @@
 #![warn(missing_docs)]
 
 extern crate chrono;
-extern crate regex;
-extern crate serde;
-#[macro_use]
-extern crate serde_derive;
 extern crate failure;
-extern crate serde_json;
-extern crate uuid;
 #[macro_use]
 extern crate failure_derive;
 #[macro_use]
 extern crate lazy_static;
+extern crate regex;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+extern crate uuid;
 
 #[macro_use]
 extern crate sentry_stripping_derive;
 
-pub use {
-    chunk::*,
-    common::*,
-    meta::*,
-    protocol::*,
-    processor::*,
-    rule::*,
-    value::*
-};
+#[macro_use]
+mod macros;
 
 mod chunk;
 mod common;
 mod meta;
+mod meta_ser;
+mod processor;
 mod protocol;
 mod rule;
-mod value;
-mod meta_ser;
 mod tracked;
 mod utils;
-mod processor;
+mod value;
+
+pub use {chunk::*, common::*, meta::*, processor::*, protocol::*, rule::*, value::*};
